0a09a23b300bf6860268a8565a8662d4a57892ba
abontikasara-spec <abontikasara@gmail.com>
2026-04-25 13:27:38 +0600

    Add files via upload

---
+++ Welcome to the world of Programming .py
@@ -0,0 +1,11 @@
+# Print a sentence
+print("Welcome to the world of programing")
+
+# Print a number
+print(5)
+
+print("Hello Python /n")
+
+print("Keywords in python are ...")
+import keyword
+print(keyword.kwlist)
