257b75f03b9a2e63078ef2141460ea5cc9194e2f
abontikasara-spec <abontikasara@gmail.com>
2026-04-30 17:11:30 +0600

    Add files via upload

---
+++ String Operations.py
@@ -0,0 +1,21 @@
+num1 = 10
+num2 = 5
+
+print("Number1 :", num1)
+print("Number2 :", num2)
+print("Addition :", num1+num2)
+print("Subtracttion :", num1-num2)
+print("Multiplition :", num1*num2)
+print("Division :", num1/num2)
+print("Floor division :", num1//num2)
+print("Modulus operation :", num1%num2)
+print("Square :",num2**2)
+print("Square Root:",num1**0.5)
+
+print("Equal ? ", num1==num2)
+print("Number 1 greater ? :", num1>num2)
+print("Number 2 greater ? :", num1<num2)
+print("Not equal ? ", num1!=num2)
+
+result = num1/2+num2**2+10
+print("The Result of the given calculation is :" , result)
